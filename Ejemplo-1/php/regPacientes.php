26f9b3df2c2f7454040eb396246bd7824df6b28c
JuanDrd <juandawey64@gmail.com>
2021-10-24 12:53:20 -0500

    publicacion de pacientes

--- Ejemplo-1/php/regPacientes.php
+++ Ejemplo-1/php/regPacientes.php
@@ -1,75 +1,52 @@
 <?php
 
-	$db_host ="localhost";
-	$db_user = "root";
-	$db_pass = "";
-	$db_name = "examen2";
+	# Las variables de la cadena de conexión
 
-	$conexion = mysql_connect($db_host,$db_user,$db_pass) or die("Error No. :" . mysql_errno() ."</br>Descripción : " . mysql_error());
+	$db_host = "localhost"; # La maquína en donde está MySQL
+	$db_user = "root"; 		# Usuario en MySQL
+	$db_pass = ""; 			# Clave del usuario
+	$db_name = "examen2"; 	# El nombre de la BD
 
-	mysql_select_db($db_name,$conexion);
+	#1. Establecer una conexión
+	$conexion = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
-	$sql = "select * from medic order by id_usr, nombre, cedula";
+	/*echo "<pre>";
+	var_dump($conexion);
+	echo "</pre>"; */
 
-	$resultado = mysql_query($sql);
+	#2. Definir la acción a realizar (SQL)
+	$sql = "SELECT c.comu_codi, c.comu_nomb, m.muni_nomb "
+			." FROM tb_comuna AS c"
+			." INNER JOIN tb_municipio AS m "
+			." ON (c.muni_codi = m.muni_codi) "
+			." ORDER BY comu_nomb " ;
+
+	#3. Ejecutar la sentencia
+	$resultado = $conexion->query($sql)
+		or die(mysqli_errno($conexion) . " : "
+			. mysqli_error($conexion) . " | Query=" . $sql);
+		
+
+	/*echo "<pre>";
+	var_dump($resultado);
+	echo "</pre>"; */
+
+	#4. Gestionar el resultado
+	/* $primerRegistro = $resultado->fetch_assoc();
+	echo "<pre>";
+	print_r($primerRegistro);
+	echo "</pre>";
+	*/
 	
-	$primerRegistro = mysql_fetch_assoc($resultado);
-
-	print_r($primerRegistro);	
-
-	$listado =array();
-	while($fila = mysql_fetch_array($resultado)){
-			$listado[]=$fila;
+	$listado = array();
+	while ($fila = $resultado->fetch_assoc()) {
+		$listado[] = $fila;
 	}
+	/*echo "<pre>";
+	var_dump($listado);
+	echo "</pre>";*/
+	echo json_encode($listado);
 
-	mysql_close($conexion);
+	#5. Cerrarl la conexión
+	$conexion->close();
 ?>
-
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>Listado de Comunas</title>
-		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-    	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.11/css/dataTables.bootstrap.min.css">
-    	<!-- Font Awesome -->
-    	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-	</head>
-	<body>
-		<h1 align="center">Usuario</h1>
-		<table id="tabla" class="table table-striped table-bordered table-hover" cellspacing="0" width="100%">			
-			<thead>
-				<tr>
-					<th>Código</th>
-					<th>Nombre</th>
-					<th>Cedula</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php foreach($listado as $fila){ ?>
-				<tr>
-					<td><?php echo $fila['id_usr'] ?> </td>
-					<td><?php echo utf8_encode($fila['nombre']) ?> </td>
-					<td><?php echo utf8_encode($fila['cedula']) ?> </td> 
-				</tr>
-				<?php } ?>
-			</tbody>
-		</table>
-
-		<!-- jQuery 2.1.4 -->
-	    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-beta1/jquery.js"></script>
-	    <script src="https://cdn.datatables.net/1.10.11/js/jquery.dataTables.min.js"></script>
-	    <script src="https://cdn.datatables.net/1.10.11/js/dataTables.bootstrap.min.js"></script>
-	    <!-- jQuery UI 1.11.4 -->
-	    <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
-	    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-	    
-	    <!-- Bootstrap 3.3.5 -->
-    	<script src="bootstrap/js/bootstrap.min.js"></script>
-    	<!-- Funciones de Lógica de neogcio -->
-		<script>
-    		$(document).ready(function(){
-    			//$("#tabla").DataTable();
-    		});
-		</script>
-	</body>
-</html>
