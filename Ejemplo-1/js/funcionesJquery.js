2c60d33f06fb840ba17dbf7a6716d56653eb5c1b
Hilary Paz <hila.p.30.18@gmail.com>
2021-10-24 13:21:24 -0500

    con registro de pacientes

--- Ejemplo-1/js/funcionesJquery.js
+++ Ejemplo-1/js/funcionesJquery.js
@@ -1,15 +1,7 @@
 
 function operaciones(){
-  
-    $("#cargarRP").click(function(){
-        $("#contenido").load("html/regPacientes.html");
-    })
 
-    $("#cargarP2").click(function(){
-        $("#contenido").load("html/historiaPacientes.html");
-    })
-
-    $("#cargarP3").click(function(){
+    $("#cargarTabla").click(function(){
         $.get("php/listadoComunaJson.php")
         .done(function( resultado ) {
             console.log("Data Loaded: " + resultado );
--- Ejemplo-1/php/regPacientes.php
+++ Ejemplo-1/php/regPacientes.php
@@ -1,3 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../bootstrap/css/estilos.css">
+    <title>Registro de Pacientes</title>
+</head>
+<body>
+
+
+ <div id="titulo" class="container">
+    <h1>Registrar paciente</h1>
+ </div>
+
+ <div class="container">
+    <form id="fdatos" action="" method="POST">
+        <div class="mb-3">
+            <label class="form-label">Nombre Paciente</label>
+            <input type="" class="form-control" id="nombre_pac" name= "nombre" placeholder="Ingrese el nombre del paciente" aria-describedby="emailHelp">
+            
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Cedula Paciente</label>
+            <input type="" class="form-control" id="cc_pac" name= "cedula" placeholder="Ingrese numero de documento">
+        
+        </div>
+    
+        <button type="submit" class="btn btn-primary" id="btnRegistrar"  name="registrar">Registrar</button>
+		<a  class="btn btn-danger"  href="../index.html" role="button">Salir</a>
+    </form>
+
+ </div>
+
+</body>
+</html>
 <?php
 
 	# Las variables de la cadena de conexión
@@ -14,39 +53,18 @@
 	var_dump($conexion);
 	echo "</pre>"; */
 
-	#2. Definir la acción a realizar (SQL)
-	$sql = "SELECT c.comu_codi, c.comu_nomb, m.muni_nomb "
-			." FROM tb_comuna AS c"
-			." INNER JOIN tb_municipio AS m "
-			." ON (c.muni_codi = m.muni_codi) "
-			." ORDER BY comu_nomb " ;
-
-	#3. Ejecutar la sentencia
-	$resultado = $conexion->query($sql)
-		or die(mysqli_errno($conexion) . " : "
-			. mysqli_error($conexion) . " | Query=" . $sql);
-		
-
-	/*echo "<pre>";
-	var_dump($resultado);
-	echo "</pre>"; */
-
-	#4. Gestionar el resultado
-	/* $primerRegistro = $resultado->fetch_assoc();
-	echo "<pre>";
-	print_r($primerRegistro);
-	echo "</pre>";
-	*/
+	if (isset($_POST['registrar'])) {
+		$nombre = $_POST['nombre'];
+		$cedula = $_POST['cedula'];
 	
-	$listado = array();
-	while ($fila = $resultado->fetch_assoc()) {
-		$listado[] = $fila;
-	}
-	/*echo "<pre>";
-	var_dump($listado);
-	echo "</pre>";*/
-	echo json_encode($listado);
+		$ingresarDatos = "INSERT INTO usr(nombre, cedula) VALUES ('$nombre','$cedula')";
+		
+		mysqli_query($conexion,$ingresarDatos);
+		
+		$conexion->close();
+		
+		}  
 
-	#5. Cerrarl la conexión
-	$conexion->close();
+
+
 ?>
